b2fb2cab8fdf89dba739d1b8271a799cb397d21b
ndela-ntu <ntulilindelani4@gmail.com>
2025-06-14 20:40:39 +0200

    changed app name and added categories

--- components/layout/side-nav.tsx
+++ components/layout/side-nav.tsx
@@ -4,7 +4,7 @@ export default function SideNav() {
   return (
     <div className="fixed h-full w-72 flex flex-col items-center border-r-2 border-black bg-white z-10">
       <div className="w-full flex flex-col items-center justify-center py-1">
-        <h1 className="text-eerieBlack text-3xl pb-1 font-black italic">SDWADLO.CO</h1>
+        <h1 className="text-eerieBlack text-3xl pb-1 font-black italic">SDWADLO</h1>
         <div className="flex w-full px-1">
           <NavLinks />
         </div>
